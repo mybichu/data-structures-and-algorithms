69e3bba7bedbcbd9637a4c4e5cccea42795a7c9f
neoyu <neoyu@tencent.com>
2022-10-29 11:33:34 +0800

    feat:init

---
+++ src/test/java/com/neoyu/datastructuresandalgorithms/DataStructuresAndAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoyu.datastructuresandalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresAndAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
